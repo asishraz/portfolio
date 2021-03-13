ffc56d0aca0c455bff92f5a536a028dfd1ad4bee
asishraz <iamasish3001@gmail.com>
2021-03-13 12:20:59 +0530

    added basic structure for abhishek

---
+++ src/components/Navigation/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { makeStyles, Grid, Typography } from "@material-ui/core";
+import Aux from "hoc/Auxiliary";
+
+const Navigation = () => {
+  return <Aux>demo</Aux>;
+};
+
+export default Navigation;
