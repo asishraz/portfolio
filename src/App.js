7886fe99f8c79ab71d14d33a8f1153c7041c5b24
asishraz <iamasish3001@gmail.com>
2021-04-28 17:24:19 +0530

    added more files and logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./components/Hero";
+import Specializing from "./components/Specializing";
 
 const App = (props) => {
   return (
     <div>
       <Hero />
+      <Specializing />
     </div>
   );
 };
