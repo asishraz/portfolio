32ffe50d4767ace3a299e1072a8c34728a5aa08c
asishraz <iamasish3001@gmail.com>
2021-06-25 17:06:19 +0530

    basic component structure done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import About from '../About/About';
+import Contact from '../Contact/Contact';
+import Projects from '../Projects/Projects';
+import Skills from '../Skills/Skills';
+import Home from '../Home/Home';
+
 
 
 const App = () => {
     return (
         <div>
-            <h1>Hello from App</h1>
+            <BrowserRouter>
+                <Route path="/" component={Home}/>
+                <Route path="/about" component={About}/>
+                <Route path="/contact" component={Contact}/>
+                <Route path="/projects" component={Projects}/>
+                <Route path="/skills" component={Skills}/>
+                
+        
+            </BrowserRouter>
+        
+            
         </div>
     );
 }
