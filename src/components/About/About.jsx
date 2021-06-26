00108454ec031741f389babb3e8b7057addfb6ff
asishraz <iamasish3001@gmail.com>
2021-06-26 22:50:24 +0530

    file added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-            <h1>From about</h1>
-        </div>
-    )
-
-}
+  return (
+    <div>
+      <h1>From about</h1>
+    </div>
+  );
+};
 
 export default About;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,26 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import About from '../About/About';
-import Contact from '../Contact/Contact';
-import Projects from '../Projects/Projects';
-import Skills from '../Skills/Skills';
-import Home from '../Home/Home';
-
-
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import About from "../About/About";
+import Contact from "../Contact/Contact";
+import Projects from "../Projects/Projects";
+import Skills from "../Skills/Skills";
+import Home from "../Home/Home";
+import "./App.css";
+import NavBar from "../NavBar/NavBar";
 
 const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <Route path="/" component={Home}/>
-                <Route path="/about" component={About}/>
-                <Route path="/contact" component={Contact}/>
-                <Route path="/projects" component={Projects}/>
-                <Route path="/skills" component={Skills}/>
-                
-        
-            </BrowserRouter>
-        
-            
-        </div>
-    );
-}
+  return (
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/skills" component={Skills} />
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
