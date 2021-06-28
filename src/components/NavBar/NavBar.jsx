b7168b36c348521319da7b6e04049bfbe6548033
asishraz <iamasish3001@gmail.com>
2021-06-28 14:38:58 +0530

    navbar styling done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,38 @@
 import React from "react";
-
+import "./NavBar.css";
 import { slide as Menu } from "react-burger-menu";
+import { Link, withRouter } from "react-router-dom";
 
 class NavBar extends React.Component {
   render() {
+    const { location } = this.props;
+    console.log(location.pathname);
+
+    const homeclass = location.pathname === "/" ? "active-item" : "";
+    const aboutclass = location.pathname === "/about" ? "active-item" : "";
+    const contactclass = location.pathname === "/contact" ? "active-item" : "";
+    const projectsclass =
+      location.pathname === "/projects" ? "active-item" : "";
+    const skillsclass = location.pathname === "/skills" ? "active-item" : "";
     return (
       <Menu>
-        <a id="home" className="menu-item" href="/">
+        <Link to="/" className={`menu-item ${homeclass}`}>
+          {" home"}
+        </Link>
+        <Link to="/about" className={`menu-item ${aboutclass}`}>
+          {" about"}
+        </Link>
+        <Link to="/contact" className={`menu-item ${contactclass}`}>
+          {" contact"}
+        </Link>
+        <Link to="/projects" className={`menu-item ${projectsclass}`}>
+          {" projects"}
+        </Link>
+        <Link to="/skills" className={`menu-item ${skillsclass}`}>
+          {" skills"}
+        </Link>
+
+        {/* <a id="home" className="menu-item" href="/">
           Home
         </a>
         <a id="about" className="menu-item" href="/about">
@@ -14,10 +40,10 @@ class NavBar extends React.Component {
         </a>
         <a id="contact" className="menu-item" href="/contact">
           Contact
-        </a>
+        </a> */}
       </Menu>
     );
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
